9a631114bd8f62dc5dd6301966fd9de87871a82b
webzakimbo <guillaume@webzakimbo.com>
2017-04-21 17:56:50 +0100

    Can now fully traverse the hierarchy up to an inventory level

--- lib/node/base.rb
+++ lib/node/base.rb
@@ -6,6 +6,9 @@ module Bcome::Node
     include Bcome::Context
     include Bcome::CommonWorkspaceCommands
 
+    INVENTORY_KEY = "inventory"
+    COLLECTION_KEY = "collection"
+
     def initialize(params)
       view_data = params[:view_data]
 
@@ -24,8 +27,33 @@ module Bcome::Node
       "#{parent.prompt_breadcrumb}> #{identifier}"
     end
 
-    def load_resources
-      raise "Should be overidden"
+    def create_tree(views, top_level = false)
+      views.each do |view|
+        raise ::Bcome::Exception::InvalidEstateConfig.new unless is_valid_view_type?(view["type"])
+        raise ::Bcome::Exception::NoInventoriesAtTopLevel.new if top_level && view["type"] == INVENTORY_KEY
+        klass = klass_for_view_type[view["type"]]
+        view_instance = klass.new({
+          :view_data => view,
+          :parent => self
+        })
+
+        if sub_views = view["views"]
+          view_instance.create_tree(sub_views)
+        end
+
+        @resources << view_instance
+      end     
+    end
+
+    def klass_for_view_type
+      {
+        COLLECTION_KEY => ::Bcome::Node::Collection,
+        INVENTORY_KEY => ::Bcome::Node::Inventory
+      } 
+    end
+
+    def is_valid_view_type?(view_type)
+      klass_for_view_type.keys.include?(view_type)
     end
 
   end
--- lib/node/estate.rb
+++ lib/node/estate.rb
@@ -1,9 +1,8 @@
 module Bcome::Node
   class Bcome::Estate < Bcome::Node::Base
 
-    CONFIG_PATH="config/bcome/estate.yml"
-
-    ESTATE_IDENTIFIER="bcome"
+    CONFIG_PATH = "config/bcome/estate.yml"
+    ESTATE_IDENTIFIER = "bcome"
 
     class << self
       def init
@@ -20,35 +19,13 @@ module Bcome::Node
       
       views = config["views"]
       raise ::Bcome::Exception::NoConfiguredViews.new if (!views && !views.is_a?(Array)) || views.empty?
- 
-      top_level = true
+
+      top_level = true 
       create_tree(views, top_level) 
     end
 
     def prompt_breadcrumb
       ::START_PROMPT
-    end
-
-    def create_tree(views, top_level = false)
-      views.each do |view|
-        raise ::Bcome::Exception::InvalidEstateConfig.new unless is_valid_view_type?(view["type"])
-        klass = klass_for_view_type[view["type"]]
-        @resources << klass.new({
-          :view_data => view,
-          :parent => self
-        })
-      end     
-    end
-
-    def klass_for_view_type
-      {
-        "collection" => ::Bcome::Node::Collection,
-        "inventory" => ::Bcome::Node::Inventory
-      } 
-    end
-
-    def is_valid_view_type?(view_type)
-      klass_for_view_type.keys.include?(view_type)
     end
 
   end
