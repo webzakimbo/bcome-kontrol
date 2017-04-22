78499195fead3161f3c9be2c5bf94144d145c72f
Guillaume Roderick <guillaume@GuillaumesAir2.home>
2017-04-22 22:56:09 +0100

    Much simplified colour scheme mechanism added - palette with highlight option:  string.colour & string.colour(:highlight)

--- lib/bcome.rb
+++ lib/bcome.rb
@@ -5,4 +5,6 @@ require 'net/ssh/proxy/command'
 require 'fog/aws'
 require 'require_all'
 require 'pmap'
+
+require_all "#{File.dirname(__FILE__)}/../patches"
 require_all "#{File.dirname(__FILE__)}/../lib"
---
+++ patches/string.rb
@@ -0,0 +1,47 @@
+class String
+  # with thanks to http://simianuprising.com/wp-content/uploads/2012/08/solarized-reference-horizontal.png
+  def colour_codes
+    {
+      :yellow => "#b58900",
+      :orange => "#cb4b16",
+      :red => "#dc322f",
+      :magenta => "#d33682",
+      :violet => "#6c71c4",
+      :blue => "#268bd2",
+      :cyan => "#2aa198",
+      :green => "#859900",
+      :white => "#ffffff"
+    }
+  end
+
+  def highlight_colour_codes
+    {
+      :yellow => "#002b36",
+      :orange => "#073642",
+      :red => "#586e75",
+      :magenta => "#657b83",
+      :violet => "#839496",
+      :blue => "#93a1a1",
+      :cyan => "#eee8d5",
+      :green => "#fdf6e3"
+    }
+  end
+
+  def method_missing(method_sym, *arguments, &black)
+    unless colour_code = colour_codes[method_sym]
+      super
+    else
+      if (arguments[0] == :highlight) && highlight_colour = highlight_colour_codes[method_sym]
+        return Rainbow(self).color(highlight_colour).bg(colour_code)
+      else
+        return Rainbow(self).color(colour_code)
+      end
+    end
+  end
+
+  def swatch
+    colour_codes.keys.each do |colour_code|
+      puts "#{colour_code}:  #{send(colour_code)} #{send(colour_code, :highlight)}"
+    end
+  end
+end
