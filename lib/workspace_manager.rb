73342be5330852682b68fa1ccdaa17568874430d
webzakimbo <guillaume@webzakimbo.com>
2017-04-25 09:59:10 +0100

    added some comments

--- lib/workspace_manager.rb
+++ lib/workspace_manager.rb
@@ -11,22 +11,25 @@ class ::Bcome::WorkspaceManager
     starting_context = ::Bcome::Estate.init_tree
 
     if context_crumbs.empty?
+      # Direct console access at the Estate node level
       BCOME.set({ :context => starting_context })
     else
       context_crumbs.each do |crumb|
         next_context = starting_context.resource_for_identifier(crumb)
         unless next_context
-          # Could not find a resource for this context, so it may be a method
+          # Execute the final crumb as a method call on our penultimate context e.g. foo:bar:ssh
           if starting_context.respond_to?(crumb)
             starting_context.send(crumb)
             return
           else
+            # Final crumb is neither a node level context nor an executable method on the penultimate node level context
             puts "No method #{crumb} available for #{starting_context.class} at namespace #{starting_context.namespace}"
             return
           end
         end
         starting_context = next_context 
       end
+      # Our final crumb relates to context, and so enter the console at this node level
       BCOME.set({ :context => starting_context })
     end  
   end
