9f08d2514eac3bb4a84358a2cd473ec00b869c26
webzakimbo <guillaume@webzakimbo.com>
2017-05-02 17:42:56 +0100

    prelim commit

--- lib/models/node/estate.rb
+++ lib/models/node/estate.rb
@@ -1,25 +1,9 @@
 module Bcome::Node
   class Estate < Bcome::Node::Base
 
-    CONFIG_PATH = "config/bcome/estate.yml"
-
-    class << self
-      def init_tree
-        config = YAML.load_file(CONFIG_PATH).deep_symbolize_keys
-
-        config[:identifier] = config[:identifier] ? config[:identifier] : "bcome"
-        config[:description] = config[:description] ? config[:description] : "Your estate"
-        config[:type] = config[:type] ? config[:type] : "collection"
-
-        estate = new(:view_data => config)
-        estate.create_tree(config[:views]) if config[:views] && config[:views].any?
-        return estate
-      end
-
-      def to_s
-        "estate"
-      end
-    end  
+    def self.to_s
+      'estate'
+    end
 
     def prompt_breadcrumb
       @identifier
