dbabaf1c04586b83324de8bd0a2a5ae9c8921b25
Guillaume Roderick <guillaume@GuillaumesAir2.home>
2017-06-20 11:36:48 +0100

    tidy around save messages

--- lib/models/interactive/session_item/capture_input.rb
+++ lib/models/interactive/session_item/capture_input.rb
@@ -6,16 +6,21 @@ module Bcome::Interactive::SessionItem
     end
 
     def terminal_prompt
-      "enter manifest file name or hit enter to save as existing (#{@init_data[:current_filename]}): ".bc_yellow
+      "enter manifest file name or hit enter to save as existing (#{display_file_name}): ".bc_magenta
+    end
+
+    def display_file_name
+      @init_data[:current_filename] =~ /(.+)\.yml/
+      return $1
     end
 
     def action
       input = get_input
-      return input.empty? ? @init_data[:current_filename] : input
+      return input.empty? ? @init_data[:current_filename] : "#{input}.yml" 
     end
 
     def start_message
-      "\n\n" + @init_data[:start_message].bc_green + "\n"
+      "\n" + @init_data[:start_message].bc_yellow.bold
     end
 
   end
