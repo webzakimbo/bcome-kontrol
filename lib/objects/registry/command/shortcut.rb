a2a5ffae00ff7f5412e7507196720ed7c5d57afe
webzakimbo <guillaume@webzakimbo.com>
2018-01-03 11:13:16 +0000

    Shortcut commands can now be run as a pseudo tty

--- lib/objects/registry/command/shortcut.rb
+++ lib/objects/registry/command/shortcut.rb
@@ -3,10 +3,22 @@ module Bcome::Registry::Command
     # In which the bcome context is a shortcut to a more complex command
 
     def execute(node, arguments)  ## We'll add in arguments later
-      node.run @data[:shortcut_command]
+      if run_as_pseudo_tty?
+        node.pseudo_tty command
+      else
+        node.run command
+      end
     rescue Interrupt
       puts "\nExiting gracefully from interrupt\n".warning
     end
 
+    def command
+      @data[:shortcut_command]
+    end
+ 
+    def run_as_pseudo_tty?
+      @data[:run_as_pseudo_tty]
+    end
+ 
   end
 end
