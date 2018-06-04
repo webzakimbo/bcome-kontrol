edb7275c590a05a6dc9d92c33a10b8ac6ba1406c
webzakimbo <guillaume@webzakimbo.com>
2018-06-04 16:41:47 +0100

    preparation for metadata encryption: loader prompts for a decryption key. Bcome will provide an interface onto an encryption mechanism

--- lib/objects/node/meta_data_loader.rb
+++ lib/objects/node/meta_data_loader.rb
@@ -8,6 +8,10 @@ module Bcome::Node
       @all_metadata_filenames = Dir["#{META_DATA_FILE_PATH_PREFIX}/*"]
     end
 
+    def decryption_key
+      @decryption_key
+    end
+ 
     def data
       @data ||= do_load
     end
@@ -16,11 +20,29 @@ module Bcome::Node
       data[namespace.to_sym] ? data[namespace.to_sym] : {}
     end
 
+    def prompt_for_decryption_key
+      message = "Please enter your metadata encryption key: ".informational
+      @decryption_key = ::Readline.readline("\n#{message}", true).squeeze('').to_s
+    end
+
+    def load_file_data_for(filepath)
+      if filepath =~ /-enc/  # encrypted file contents 
+        prompt_for_decryption_key unless decryption_key
+        encrypted_contents = File.read(filepath)     
+        decrypted_contents = encrypted_contents.decrypt(decryption_key) 
+        return YAML.load(decrypted_contents)
+      else # unencrypted
+        return YAML.load_file(filepath)
+      end
+    end
+
     def do_load
       all_meta_data = {}
-      @all_metadata_filenames.each do |filename|
+      @all_metadata_filenames.each do |filepath|
+        next if filepath =~ /-unenc/  # we only read from the encrypted, packed files. 
+
         begin
-          filedata = YAML.load_file(filename)
+          filedata = load_file_data_for(filepath)
           all_meta_data.deep_merge!(filedata)
         rescue Psych::SyntaxError => e
           raise Bcome::Exception::InvalidMetaDataConfig, "Error: #{e.message}"
