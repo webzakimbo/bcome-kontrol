25b50de52cde2931c04a0eedce4eda03cdeb8f6c
webzakimbo <guillaume@webzakimbo.com>
2018-09-13 17:01:14 +0100

    config user has precendence over local user override

--- lib/objects/ssh/driver.rb
+++ lib/objects/ssh/driver.rb
@@ -130,11 +130,11 @@ module Bcome::Ssh
       # If we're in bootstrapping mode and have a bootstrap user set, return it
       return @bootstrap_settings.user if (bootstrap? && @bootstrap_settings.user)
 
-      # If the local user has explicitly overriden their user, return that
-      return overriden_local_user if overriden_local_user
-
       # If we have a user explcitly set in the config, then return it
       return @config[:user] if @config[:user]
+
+      # If the local user has explicitly overriden their user, return that
+      return overriden_local_user if overriden_local_user
 
       # Else fall back to whichever local user is using bcome
       fallback_local_user
