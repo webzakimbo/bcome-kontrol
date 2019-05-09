f8b29ae01e8d557f2024426c8b03b12bbfd0ad1b
webzakimbo <guillaume@webzakimbo.com>
2019-05-09 16:29:16 +0100

    1. Added to TODO, 2. Tidied GCP driver. 3. GCP region & project now comes from the network config, making use of config inheritance model

--- lib/objects/driver/gcp.rb
+++ lib/objects/driver/gcp.rb
@@ -8,27 +8,23 @@ module Bcome::Driver
     TOKEN_PATH = 'token.yaml'.freeze
 
     # Read-only scope for now. We're only interested in listing
-    # Full list of scopes: /Users/guillaume/.rvm/gems/ruby-2.3.3@gcp-scratch/gems/google-api-client-0.29.1/generated/google/apis/compute_beta.rb
-    # TODO - scope should be user determined, as the gcp driver for any given namespace will be made available for scripting purposes.
+    # Full list of scopes: /your/gem/path/google-api-client-0.29.1/generated/google/apis/compute_beta.rb
     SCOPE = ::Google::Apis::ComputeBeta::AUTH_COMPUTE_READONLY
-
  
     def initialize(*params)
       super
     end
 
     def fetch_server_list(filters)
-      # TODO - fix hardcoded project & zone: this will come from the networks config 
-      project = "webzakimbo-spike" ## TODO use bcome's inheritance model
-      zone = "europe-west2-c" ## TODO "
-      instances = gcp_client.list_instances(project, zone)
+      instances = gcp_client.list_instances(@params[:project], @params[:zone])
       instances.items
     end
 
+    # TODO - unravel this flow; oauth will be triggered which is not what we want.
     def set_gcp_client
       client = ::Google::Apis::ComputeBeta::ComputeService.new
       client.client_options.application_name = APPLICATION_NAME
-      client.authorization = authorize  ## TODO - currently will break flow when Oauth triggered
+      client.authorization = authorize  
       client
     end   
  
@@ -38,12 +34,8 @@ module Bcome::Driver
 
     protected
 
-    ##
-    # Ensure valid credentials, either by restoring from the saved credentials
-    # files or intitiating an OAuth2 authorization. If authorization is required,
-    # the user's default browser will be launched to approve the request.
-    #
-    # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+    # Taken from google's gdrive authentication examples
+    # TODO - unravel this flow
     def authorize
       client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
       token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
@@ -60,10 +52,6 @@ module Bcome::Driver
       end
       credentials
     end
-
-
-
-
 
   end
 end
