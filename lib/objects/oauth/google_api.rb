b750b2cafd82e0ebf5e2c261f132c2de68d7a093
webzakimbo <guillaume@webzakimbo.com>
2019-05-21 16:12:32 +0100

    Secrets path now comes from network config. This allows multiple GCP accounts to be configured within the same project

--- lib/objects/oauth/google_api.rb
+++ lib/objects/oauth/google_api.rb
@@ -3,10 +3,11 @@ module Bcome::Oauth
 
     CREDENTIAL_FILE_SUFFIX = "oauth2.json".freeze
 
-    def initialize(service, scopes, node)
+    def initialize(service, scopes, node, path_to_secrets)
       @service = service
       @scopes = scopes
       @node = node
+      @path_to_secrets = path_to_secrets
     end
 
     def storage
@@ -22,21 +23,34 @@ module Bcome::Oauth
     end
 
     def client_secrets
-      @client_secrets ||= ::Google::APIClient::ClientSecrets.load
+      @client_secrets ||= load_client_secrets
+    end
+
+    def load_client_secrets
+      begin
+        ::Google::APIClient::ClientSecrets.load(@path_to_secrets)
+      rescue Exception
+        raise ::Bcome::Exception::MissingOrInvalidClientSecrets, "#{@path_to_secrets}. Gcp exception: #{e.class} #{e.message}"
+      end
     end
 
     def do!
       authorize!
       if @storage.authorization.nil?
         print "\nAuthenticating with GCP for network namespace #{@node.namespace}. Close browser window once done. A GCP secrets file named '#{credential_file}' will be placed in your project root.".informational
-        print "\sDo not commit this file to source control".warning
+        print "\sDo not commit this file to source control\n".warning
 
         flow = Google::APIClient::InstalledAppFlow.new(
           :client_id => client_secrets.client_id,
           :client_secret => client_secrets.client_secret,
           :scope => @scopes
         )
-        @service.authorization = flow.authorize(storage)
+        begin
+          @service.authorization = flow.authorize(storage)
+        rescue ArgumentError => e 
+          raise ::Bcome::Exception::MissingOrInvalidClientSecrets, "#{@path_to_secrets}. Gcp exception: #{e.class} #{e.message}"
+        end
+
       end
       return @service
     end
