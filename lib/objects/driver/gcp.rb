6e53c71331d6a82ef46a52b85c871231c682b903
webzakimbo <guillaume@webzakimbo.com>
2019-05-22 13:39:55 +0100

    Setting the scene for three methods of authentication against GCP: Oauth, service accounts, API keys

--- lib/objects/driver/gcp.rb
+++ lib/objects/driver/gcp.rb
@@ -6,6 +6,7 @@ module Bcome::Driver
     def initialize(*params)
       super
       validate_service_scopes
+      validate_authentication_scheme
     end
 
     def fetch_server_list(filters)
@@ -26,12 +27,37 @@ module Bcome::Driver
 
     protected
 
+    def validate_authentication_scheme
+      raise ::Bcome::Exception::MissingGcpAuthenticationScheme.new "node #{@node.namespace}" if @params[:authentication_scheme].nil? || @params[:authentication_scheme].empty?
+      raise ::Bcome::Exception::InvalidGcpAuthenticationScheme.new "Invalid GCP authentication scheme '#{@params[:authentication_scheme]}' for node #{@node.namespace}" unless auth_scheme
+    end
+
+    def invalid_auth_scheme?
+      !auth_schemes.keys.include?(@params[:authentication_scheme].to_sym)
+    end
+
+    def auth_scheme
+      auth_schemes[@params[:authentication_scheme].to_sym]
+    end
+
+    def auth_schemes
+      {
+        :oauth => :gcp_authenticate_via_oauth, 
+        :serviceaccount => :gcp_authenticate_via_service_account
+      }
+    end 
+
     def get_gcp_compute_service
-      gcp_compute_authenticate!
+      send(auth_scheme)
       gcp_service
     end
 
-    def gcp_compute_authenticate!
+    def gcp_authenticate_via_service_account
+      ## TODO - placeholder for service account authentication
+      gcp_service
+    end
+
+    def gcp_authenticate_via_oauth
       # Service scopes are now specified directly from the network config
       # A minumum scope of https://www.googleapis.com/auth/compute.readonly is required in order to list resources.
       authentication = ::Bcome::Oauth::GoogleApi.new(gcp_service, service_scopes, @node, @params[:secrets_path])
