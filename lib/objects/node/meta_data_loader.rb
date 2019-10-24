773da0780936c6a6439c93d149b6f283dd4d5aea
guillaume <guillaume@guillaumes-MacBook-Pro.local>
2019-10-24 11:43:57 +0100

    Outrageous: rubocop changes YAML.load  to YAML.safe_load. Safe load is buggy AF in latest rubies

--- lib/objects/node/meta_data_loader.rb
+++ lib/objects/node/meta_data_loader.rb
@@ -53,7 +53,7 @@ module Bcome::Node
         decrypted_contents = encrypted_contents.decrypt(decryption_key)
 
         begin
-          YAML.safe_load(decrypted_contents) # NOTE: safe_load is buggy and incorrectly parses the yaml.
+          YAML.load(decrypted_contents) # NOTE: safe_load is buggy: It incorrectly parses the yaml where variables are used and referenced..
         rescue Exception => e
           @decryption_key = nil
           raise ::Bcome::Exception::InvalidMetaDataConfig, "#{e.class} #{e.message} - " + decrypted_contents
