6b34122b471f3904a79bcc22e82e8a2725166e31
guillaume <guillaume@guillaumes-MacBook-Pro.local>
2020-06-08 12:19:41 +0100

    added backwards support for previous gcp service account credentials file paths where path was inferred

--- lib/objects/driver/gcp/authentication/service_account.rb
+++ lib/objects/driver/gcp/authentication/service_account.rb
@@ -21,16 +21,38 @@ module Bcome::Driver::Gcp::Authentication
     end 
  
     def service_account
-      @service_account ||= ::Bcome::Driver::Gcp::Authentication::SignetServiceAccountClient.new(@scopes, full_path_to_credential_file)
+      @service_account ||= ::Bcome::Driver::Gcp::Authentication::SignetServiceAccountClient.new(@scopes, credentials_file_path)
+    end
+
+    def credentials_file_path
+      has_namespaced_keyed_filename? ? namespaced_keyed_filename : defined_credentials_files
     end
 
     def ensure_credentials_file
+      return if has_namespaced_keyed_filename?
       raise ::Bcome::Exception::MissingGcpServiceAccountCredentialsFilename unless @credentials_file_name
     end  
-  
-    def full_path_to_credential_file
+ 
+    ## New implementation - we take a defined file name for the service account credentials
+    ## Clean & may be re-used
+    def defined_credentials_files
       "#{credential_directory}/#{@credentials_file_name}"
     end
+ 
+    def has_namespaced_keyed_filename?
+      @has_namespaced_keyed_filename ||= File.exist?(namespaced_keyed_filename)
+    end
+
+    ## Older implementation - we infer the credentials file from the namespace
+    ## Retained to provide backwards compatibility
+    def namespaced_keyed_filename
+      full_path_to_credential_file
+    end
+
+    def credential_file_suffix
+      'service-account.json'
+    end
+    #######################################
 
     def authorize!
       storage.authorize
