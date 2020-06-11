90d5863ffbe096a240365059a8254f3e672c0637
guillaume <guillaume@guillaumes-MacBook-Pro.local>
2020-06-11 16:30:09 +0100

    service_account_credentials_file becomes service_account_credentials in the GCP network block

--- lib/objects/exception/missing_gcp_service_account_credentials_filename.rb
+++ lib/objects/exception/missing_gcp_service_account_credentials_filename.rb
@@ -3,7 +3,7 @@ module Bcome
   module Exception
     class MissingGcpServiceAccountCredentialsFilename < ::Bcome::Exception::Base
       def message_prefix
-        "Cannot authenticate with GCP - missing service account credentials file name in networks.yml.  Define this with a 'service_account_credentials_file' key"
+        "Cannot authenticate with GCP - missing service account credentials file name in networks.yml.  Define this with a 'service_account_credentials' key"
       end
     end
   end
